b7fc2612c922367902eca38fb05ef611d758a38d
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2024-11-30 18:03:38 +0530

    Corrected user profile endpoint

--- src/models/request/request-with-user.interface.ts
+++ src/models/request/request-with-user.interface.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
 import { JwtToken } from './jwt-token.interface';
+import { LoginGoogleUserDto } from '../user/login-google-user.dto';
 
 export interface RequestWithUser extends Request {
-  user: JwtToken;
+  user: (JwtToken | LoginGoogleUserDto);
 }
